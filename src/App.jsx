02ea04a48eab870984c455addca5fdba6b76b3fb
lPlazas <rplazas200@hotmail.com>
2025-11-10 23:18:26 -0500

    Corrige importación en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+
 
 function App() {
   return (
